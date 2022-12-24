98675232e263282cedf36b64528484246acda02e
Tsogtgerel <ts.r.madrid@gmail.com>
2022-12-24 21:43:21 +0800

    add filters

--- js/app1.js
+++ js/app1.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('li').click(function(){
     navigator.bluetooth.requestDevice({
-      filters: []
+      filters: [0x1234, 0x12345678, '99999999-0000-1000-8000-00805f9b34fb']
     })
     .then(device => { console.log(device) })
     .catch(error => { console.error(error); })  
