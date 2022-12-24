f6de8f13c291a60955e691bcffe30d0b20af94d7
Tsogtgerel <ts.r.madrid@gmail.com>
2022-12-24 21:48:03 +0800

    add filters

--- js/app1.js
+++ js/app1.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
   $('li').click(function(){
     navigator.bluetooth.requestDevice({
-      filters: [0x1234, 0x12345678, '99999999-0000-1000-8000-00805f9b34fb']
+      filters: [
+        { services: ["heart_rate"] },
+        { services:[0x1234, 0x12345678, '99999999-0000-1000-8000-00805f9b34fb']},
+        { name: "Redmi Note 11 4G" },
+        { namePrefix: "Redmi" },
+        { namePrefix: "[LG]" },
+      ]
     })
     .then(device => { console.log(device) })
     .catch(error => { console.error(error); })  
